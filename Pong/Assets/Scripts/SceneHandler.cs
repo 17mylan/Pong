6a8ac78d3859e902678501291d1d62768bc307ef
Mylan <developerspanly@icloud.com>
2022-11-11 23:08:17 +0100

    Fade

---
+++ Pong/Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+
+    [SerializeField] RectTransform fader;
+
+    private void Start()
+    {
+
+        fader.gameObject.SetActive(true);
+
+         LeanTween.alpha (fader, 1, 0);
+         LeanTween.alpha (fader, 0, 0.5f).setOnComplete (() => {
+             fader.gameObject.SetActive (false);
+        });
+    }
+}
